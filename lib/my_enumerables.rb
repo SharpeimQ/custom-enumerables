f9cae56ba5f4cef0c710ef1bf4b9c29eb472e2bc
SharpeimQ <SharpHaiyan@gmail.com>
2023-04-03 13:52:41 -0400

    Build custom add? and any? enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,18 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    each do |element|
+      return false unless yield(element)
+    end
+    true
+  end
+
+  def my_any?
+    each do |element|
+      return true if yield(element)
+    end
+    false
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +20,6 @@ end
 # your enumerable module will have access
 # to this method
 class Array
+  include Enumerable
   # Define my_each here
 end
